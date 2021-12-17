5749bad8bf445d4d7ddda57ebec043fab0287aad
Bensi5 <77843600+Bensi5@users.noreply.github.com>
2021-12-17 17:19:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	jump=loadSound("jump.wav");
+	Mcoins=loadSound("coin.wav");
+	gameover=loadSound("gameover.wav");
+	kick=loadSound("kick.wav");
+	Mdie=loadSound("moriodie.wav")
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent("canvas");
+	instializeInSetup(mario);
+
+
+	video=createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
+
+	poseNet=ml5.poseNet(video,modelLoaded);
+	poseNet.on("pose",getposes);
+}
+
+function draw() {
+	game()
+	
+	
+}
+function modelLoaded(){
+	console.log("model is loaded");
+}
+ 
+function getposes(results){
+if(results.length>0){
+	noseX=results[0].pose.nose.x;
+	noseY=results[0].pose.nose.y;
+	console.log(results);
+}
+}
+
+
+
+
+
